bde031395cbceaa4df09018acdbcbe0fa6dc620a
Anna Vester <veannaa@gmail.com>
2012-08-16 17:33:36 -0500

    updated animation, added logo and view task button, etc.

--- toodledo.js
+++ toodledo.js
@@ -1,17 +1,20 @@
 var Toodledo = (function() { 
     var d = top.document,
+        l = d.location,
+        e = encodeURIComponent,
         host = 'http://dev.toodledo.com',
         host_m = 'http://m.toodledo.com',
         quickAddUrl = 'http://beta.toodledo.com/tools/quickadd.php',
         byId=function(el) { return d.getElementById(el) };
             
     init = function () {
-        var a = byId('t_wrap'),
+        var a = byId('TDL_wrap'),
             css = d.createElement('link'),
             body = d.getElementsByTagName('body')[0],
             head = d.getElementsByTagName("head")[0],
             wrap = d.createElement('div'),
             topFrame = d.createElement('div'),
+            viewTasks = d.createElement('div'),
             iframe = d.createElement('iframe');
         
         if(a) d.body.removeChild(a);
@@ -21,21 +24,32 @@ var Toodledo = (function() {
         css.type = 'text/css';
         css.href = host + '/style.css';
         
-        wrap.id = 't_wrap';
-        topFrame.id = 't_head';
-        topFrame.innerHTML = '<h2>Toodledo.com</h2>';
+        wrap.id = 'TDL_wrap';
+        topFrame.id = 'TDL_head';
+        topFrame.innerHTML = '<a id="TDL_close">x</a><h2>Toodledo.com</h2>';
         iframe.src = quickAddUrl;
-        iframe.id = 't_iframe';
+        iframe.id = 'TDL_iframe';
         iframe.frameBorder = 0;
-        
+        viewTasks.id = 'TDL_viewTasks';
+        viewTasks.innerHTML = '<a id="TDL_openTasks"><img src="'+host+'/logo150.png" width="114" /><img src="'+host+'/viewtasks.png" /><img src="'+host+'/btnViewTasks.png" width="93" /></a>';
         body.appendChild(wrap);
                       
-        wrap.appendChild(topFrame);        
+        wrap.appendChild(topFrame); 
+        wrap.appendChild(viewTasks);       
         wrap.appendChild(iframe);
         
         show(); 
         listen( window, 'message', listener);
+        listen(byId('TDL_openTasks'), 'click', openTasks);
+        listen(byId('TDL_close'), 'click', function( event ) {
+            event.preventDefault();
+            hide();  
+        });
     };    
+    sendMessage = function() {
+        var frameContent = byId('TDL_iframe');
+        frameContent.contentWindow.postMessage("note:" + e(l.href) , "*" );    
+    };
     listen = function( obj, event, callback ) {
         if (window.addEventListener){
             obj.addEventListener(event, callback, false);
@@ -44,43 +58,31 @@ var Toodledo = (function() {
         }
     };
     listener = function() {
-        if (( event.origin == 'http://beta.toodledo.com' ) && (event.data == 'pass')) {
-           showViewAllTasks(); 
-           hide();
+        console.log(event.data);
+        if (event.data == 'pass') {
+            setTimeout(function () {
+                hide(); 
+            }, 5000 );            
+        } else if (event.data == 'ready') {
+            sendMessage();
         }
-    };
-    showViewAllTasks = function() {
-        var viewTasks = d.createElement('div'),
-            a = byId('t_wrap');
-        a.style.height = '200px';
-        a.removeChild(byId('t_iframe'));
-        a.appendChild(viewTasks);
-        viewTasks.id = 't_viewTasks';
-        
-        viewTasks.innerHTML = '<a id="openTasks">View Tasks<img src="'+host+'/viewtasks.png" /></a> <h2>Task Added.</h2>';
-        
-        listen(byId('openTasks'), 'click', openTasks); 
-        
     };
     openTasks = function () {
         window.open(host_m, '_blank', 'height=480, width=320, scrollbars=0')
     };
     show = function () {
-        var a = byId('t_wrap'),
-            b = byId('t_iframe');
+        var a = byId('TDL_wrap');
         setTimeout(function () {
             a.style.visibility = 'visible';
-            a.style.height = '250px';
-            b.style.display = 'block';  
+            a.style.top = '0';
         }, 300);      
     };
-    hide = function () {
-        var a = byId('t_wrap');
+    hide = function () {        
+        var a = byId('TDL_wrap');
+        a.style.top = '-300px';                   
         setTimeout(function () {
-            a.style.height = '0';
-            a.style.visibility = 'hidden';  
-            d.body.removeChild(a);          
-        }, 5000);
+            d.body.removeChild(a);
+        }, 300 );
     };    
     init();    
 })();
