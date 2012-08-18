3f711cec66c3645ffabb49926a019df2588ea85d
Anna Vester <veannaa@gmail.com>
2012-08-18 16:33:00 -0500

    removed grey bar, minor optimization, replaced image button with css

--- toodledo.js
+++ toodledo.js
@@ -1,56 +1,62 @@
 var Toodledo = (function() { 
-    var d = top.document,
-        l = d.location,
-        e = encodeURIComponent,
-        host = 'http://dev.toodledo.com',
-        host_m = 'http://m.toodledo.com',
-        quickAddUrl = 'http://beta.toodledo.com/tools/quickadd.php',
-        byId=function(el) { return d.getElementById(el) };
+    var d=top.document,
+        l=d.location,
+        e=encodeURIComponent,
+        host='http://dev.toodledo.com',
+        host_m='http://m.toodledo.com',
+        quickAddHost='http://beta.toodledo.com',
+        quickAddUrl=quickAddHost + '/tools/quickadd.php';
             
     init = function () {
-        var a = byId('TDL_wrap'),
-            css = d.createElement('link'),
-            body = d.getElementsByTagName('body')[0],
-            head = d.getElementsByTagName("head")[0],
-            wrap = d.createElement('div'),
-            topFrame = d.createElement('div'),
-            viewTasks = d.createElement('div'),
-            iframe = d.createElement('iframe');
+        var a=d.getElementById('TDL_wrap'),
+            css=d.createElement('link'),
+            body=d.getElementsByTagName('body')[0],
+            head=d.getElementsByTagName('head')[0],
+            wrap=d.createElement('div'),
+            viewTasks=d.createElement('div'),
+            iframe=d.createElement('iframe');
         
-        if(a) d.body.removeChild(a);
+        //closing bookmarklet if already opened
+        if(a) {
+            hide();
+            return;
+        }
         
+        //attaching css
         head.appendChild(css);
         css.rel = 'stylesheet';
         css.type = 'text/css';
         css.href = host + '/style.css';
         
+        //setting up bookmarklet structure
         wrap.id = 'TDL_wrap';
-        topFrame.id = 'TDL_head';
-        topFrame.innerHTML = '<a id="TDL_close">x</a><h2>Toodledo.com</h2>';
         iframe.src = quickAddUrl;
         iframe.id = 'TDL_iframe';
         iframe.frameBorder = 0;
         viewTasks.id = 'TDL_viewTasks';
-        viewTasks.innerHTML = '<a id="TDL_openTasks"><img src="'+host+'/logo150.png" width="114" /><img src="'+host+'/viewtasks.png" /><img src="'+host+'/btnViewTasks.png" width="93" /></a>';
-        body.appendChild(wrap);
-                      
-        wrap.appendChild(topFrame); 
+        viewTasks.innerHTML = '<a id="TDL_close">x</a><a id="TDL_openTasks"><img src="'+host+'/logo114.png" width="114" /><img src="'+host+'/viewtasks.png" /><span class="tdl_button">View Tasks</span></a>';
+        
+        body.appendChild(wrap);                      
         wrap.appendChild(viewTasks);       
         wrap.appendChild(iframe);
         
+        //displaying the bookmarklet
         show(); 
-        listen( window, 'message', listener);
-        listen(byId('TDL_openTasks'), 'click', openTasks);
-        listen(byId('TDL_close'), 'click', function( event ) {
-            event.preventDefault();
+        
+        //adding event listeners
+        listen(window, 'message', listener); //iframe message listener
+        listen(d.getElementById('TDL_openTasks'), 'click', openTasks); //opens mobile site in a new window
+        listen(d.getElementById('TDL_close'), 'click', function() {
             hide();  
         });
     };    
     sendMessage = function() {
-        var frameContent = byId('TDL_iframe');
-        frameContent.contentWindow.postMessage("note:" + e(l.href) , "*" );    
+        //sending message to display url in the notes field
+        var frameContent = d.getElementById('TDL_iframe');
+        frameContent.contentWindow.postMessage("note:" + e(l.href) , quickAddHost );    
     };
     listen = function( obj, event, callback ) {
+        //cross-browser event listener
         if (window.addEventListener){
             obj.addEventListener(event, callback, false);
         } else {
@@ -58,31 +64,53 @@ var Toodledo = (function() {
         }
     };
     listener = function() {
-        console.log(event.data);
-        if (event.data == 'pass') {
-            setTimeout(function () {
-                hide(); 
-            }, 5000 );            
-        } else if (event.data == 'ready') {
-            sendMessage();
+        var val = event.data,
+            o = event.origin;      
+            
+        if (o == quickAddHost) { //ensure that message is from our server
+            switch (val) {
+                case 'pass':
+                    setTimeout(function () {
+                        hide(); 
+                    }, 5000 ); 
+                    break;
+                case 'ready':
+                    sendMessage();
+                    break;
+                default:
+                    if(val.indexOf("height:")!==-1) {
+                        setHeight(val.substr(7));
+                    }
+            }
         }
     };
     openTasks = function () {
+        //opens a mobile site window
         window.open(host_m, '_blank', 'height=480, width=320, scrollbars=0')
     };
+    setHeight = function ( val ) {
+        //setting bookmarklet's height based on value recieved
+        var a = d.getElementById('TDL_wrap'),
+            b = d.getElementById('TDL_iframe'),
+            c = d.getElementById('TDL_openTasks'),
+            i = parseInt(val)+5; // to add more space at the bottom
+            
+        a.style.height = b.style.height = i+"px";
+        c.style.height = (i-30)+"px";
+    };
     show = function () {
-        var a = byId('TDL_wrap');
+        var a = d.getElementById('TDL_wrap');
         setTimeout(function () {
             a.style.visibility = 'visible';
             a.style.top = '0';
         }, 300);      
     };
     hide = function () {        
-        var a = byId('TDL_wrap');
+        var a = d.getElementById('TDL_wrap');         
         a.style.top = '-300px';                   
         setTimeout(function () {
             d.body.removeChild(a);
-        }, 300 );
+        }, 1000 );        
     };    
     init();    
 })();
